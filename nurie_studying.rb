33e8b7c30e87d3420422cb866930acc128f865dd
maoslele <m01.sgwk@gmail.com>
2020-08-24 23:47:53 +0900

    refactoring from sample

--- nurie_studying.rb
+++ nurie_studying.rb
@@ -1,30 +1,49 @@
-content = 0
-time = 0
-squares = 0
+require 'colorize'
 
-def input_status
-  puts "今日はどの項目を何分勉強した？？"
-  print "学習項目を入力 ＞ "
-  content = gets.chomp
-  print "学習時間を入力 ＞ "
-  time = gets.chomp.to_i
-  return content, time
+def update_logs(input_subject, input_minutes, study_logs)
+  input_subject.downcase!
+  if study_logs.has_key?(input_subject)
+    study_logs.store(input_subject, input_minutes+=input_minutes)
+  else
+    study_logs.store(input_subject, input_minutes)
+  end
 end
 
-def calc_squares(time)
-  squares = (time.to_f / 15).floor
-  return squares
+def print_logs(study_logs)
+  puts "\n********学習記録********"
+  study_logs.each {|subject, minutes|
+    squares = ""
+    (minutes / 15).times {
+      squares << "■"
+    }
+    puts "#{subject}：#{squares.green}"
+  }
+  puts "************************"
 end
 
-def show_status(content, squares)
-  puts ""
-  puts "学習記録"
-  puts ""
-  print "#{content} "
-  squares.times {|square| print "■"}
-  puts ""
+def main
+  study_logs = {}
+
+  while true do
+    input_subject = nil
+    input_minutes = nil
+
+    puts "今日はどの項目を何分勉強した？？"
+    print "学習項目を入力 ＞ "
+    input_subject = gets.chomp
+    print "学習時間を入力(単位：分) ＞ "
+    input_minutes = gets.chomp.delete("^0-9").to_i #.delete("^0-9")で0-9以外の入力値を自動削除
+
+    update_logs(input_subject, input_minutes, study_logs)
+    print_logs(study_logs)
+
+    puts "\n続けて入力しますか？(Y/Nを入力)"
+    continue = gets.chomp
+    continue.upcase!
+    return if continue == "N"
+
+  end
+
 end
 
-content, time = input_status()
-squares = calc_squares(time)
-show_status(content, squares)
+main
